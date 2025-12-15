f32e830ebbb6c213397cec53d1d574e4366e00bf
Soatok Dreamseeker <soatok.dhole@gmail.com>
2025-12-14 19:24:51 -0500

    Improve unit test coverage for webfinger

--- tests/ActivityPub/WebFingerTest.php
+++ tests/ActivityPub/WebFingerTest.php
@@ -9,13 +9,22 @@ use FediE2EE\PKD\Crypto\Exceptions\{
 };
 use FediE2EE\PKD\Crypto\SecretKey;
 use FediE2EE\PKDServer\ActivityPub\WebFinger;
-use FediE2EE\PKDServer\Exceptions\FetchException;
+use FediE2EE\PKDServer\Exceptions\{
+    CacheException,
+    DependencyException,
+    FetchException
+};
 use FediE2EE\PKDServer\AppCache;
 use FediE2EE\PKDServer\ServerConfig;
 use FediE2EE\PKDServer\Tests\HttpTestTrait;
 use GuzzleHttp\{
     Client,
     Exception\GuzzleException,
+    Exception\RequestException,
+    Handler\MockHandler,
+    HandlerStack,
+    Middleware,
+    Psr7\Request,
     Psr7\Response
 };
 use ParagonIE\Certainty\{
@@ -29,6 +38,8 @@ use PHPUnit\Framework\{
     MockObject\Exception,
     TestCase
 };
+use Psr\SimpleCache\InvalidArgumentException;
+use ReflectionMethod;
 use ReflectionProperty;
 use SodiumException;
 
@@ -70,8 +81,10 @@ class WebFingerTest extends TestCase
     }
 
     /**
+     * @return void
      * @throws CertaintyException
      * @throws SodiumException
+     * @throws DependencyException
      */
     public function testConstructorWithClient(): void
     {
@@ -84,10 +97,93 @@ class WebFingerTest extends TestCase
 
     /**
      * @throws CertaintyException
+     * @throws DependencyException
+     * @throws Exception
+     * @throws SodiumException
+     * @throws CacheException
+     */
+    public function testClearCaches(): void
+    {
+        $mockCache = $this->createMock(AppCache::class);
+        $mockCache->expects($this->exactly(3))->method('clear')->willReturn(true);
+
+        $webFinger = new WebFinger($this->getConfig());
+
+        $canonicalCacheProp = new ReflectionProperty(WebFinger::class, 'canonicalCache');
+        $canonicalCacheProp->setValue($webFinger, $mockCache);
+
+        $inboxCacheProp = new ReflectionProperty(WebFinger::class, 'inboxCache');
+        $inboxCacheProp->setValue($webFinger, $mockCache);
+
+        $pkCacheProp = new ReflectionProperty(WebFinger::class, 'pkCache');
+        $pkCacheProp->setValue($webFinger, $mockCache);
+
+        $webFinger->clearCaches();
+    }
+
+    /**
+     * @throws CertaintyException
+     * @throws DependencyException
+     * @throws SodiumException
+     */
+    public function testCacheTTLValues(): void
+    {
+        $webFinger = new WebFinger($this->getConfig());
+
+        $canonicalCacheProp = new ReflectionProperty(WebFinger::class, 'canonicalCache');
+        $canonicalCache = $canonicalCacheProp->getValue($webFinger);
+        $this->assertSame(
+            3600,
+            new ReflectionProperty(AppCache::class, 'defaultTTL')->getValue($canonicalCache)
+        );
+
+        $inboxCacheProp = new ReflectionProperty(WebFinger::class, 'inboxCache');
+        $inboxCache = $inboxCacheProp->getValue($webFinger);
+        $this->assertSame(
+            60,
+            new ReflectionProperty(AppCache::class, 'defaultTTL')->getValue($inboxCache)
+        );
+
+        $pkCacheProp = new ReflectionProperty(WebFinger::class, 'pkCache');
+        $pkCache = $pkCacheProp->getValue($webFinger);
+        $this->assertSame(
+            60,
+            new ReflectionProperty(AppCache::class, 'defaultTTL')->getValue($pkCache)
+        );
+    }
+
+    public function testExceptionCodes(): void
+    {
+        $mockHttp = $this->getMockClient([
+            new RequestException("Test Exception", new Request('GET', 'test')),
+            new RequestException("Test Exception", new Request('GET', 'test')),
+        ]);
+        $webFinger = new WebFinger($this->getConfig(), $mockHttp);
+
+        try {
+            $webFinger->getPublicKey('https://example.com/users/alice');
+            $this->fail('Expected FetchException was not thrown');
+        } catch (FetchException $e) {
+            $this->assertSame(0, $e->getCode());
+        }
+
+        try {
+            $method = new ReflectionMethod(WebFinger::class, 'getPublicKeyFromActivityPub');
+            $method->setAccessible(true);
+            $method->invoke($webFinger, 'https://example.com/users/alice');
+            $this->fail('Expected FetchException was not thrown');
+        } catch (FetchException $e) {
+            $this->assertSame(0, $e->getCode());
+        }
+    }
+
+    /**
+     * @throws CertaintyException
+     * @throws DependencyException
+     * @throws Exception
      * @throws GuzzleException
      * @throws NetworkException
      * @throws SodiumException
-     * @throws Exception
      */
     public function testCanonicalizeCache(): void
     {
@@ -116,9 +212,10 @@ class WebFingerTest extends TestCase
     /**
      * @throws CertaintyException
      * @throws CryptoException
+     * @throws DependencyException
      * @throws FetchException
-     * @throws SodiumException
      * @throws NotImplementedException
+     * @throws SodiumException
      */
     public function testGetPublicKeyMultibase(): void
     {
@@ -151,6 +248,7 @@ class WebFingerTest extends TestCase
     /**
      * @throws CertaintyException
      * @throws CryptoException
+     * @throws DependencyException
      * @throws FetchException
      * @throws NotImplementedException
      * @throws SodiumException
@@ -184,6 +282,7 @@ class WebFingerTest extends TestCase
     /**
      * @throws CertaintyException
      * @throws CryptoException
+     * @throws DependencyException
      * @throws FetchException
      * @throws SodiumException
      */
@@ -200,6 +299,7 @@ class WebFingerTest extends TestCase
     /**
      * @throws CertaintyException
      * @throws CryptoException
+     * @throws DependencyException
      * @throws FetchException
      * @throws SodiumException
      */
@@ -216,6 +316,7 @@ class WebFingerTest extends TestCase
     /**
      * @throws CertaintyException
      * @throws CryptoException
+     * @throws DependencyException
      * @throws FetchException
      * @throws SodiumException
      */
@@ -241,6 +342,7 @@ class WebFingerTest extends TestCase
     /**
      * @throws CertaintyException
      * @throws CryptoException
+     * @throws DependencyException
      * @throws FetchException
      * @throws SodiumException
      */
@@ -266,6 +368,7 @@ class WebFingerTest extends TestCase
     /**
      * @throws CertaintyException
      * @throws CryptoException
+     * @throws DependencyException
      * @throws FetchException
      * @throws SodiumException
      */
@@ -304,5 +407,137 @@ class WebFingerTest extends TestCase
         $webFinger = new WebFinger($config, $http);
         $inboxUrl = $webFinger->getInboxUrl($in);
         $this->assertSame($expect, $inboxUrl);
+    }
+
+    /**
+     * @throws CertaintyException
+     * @throws DependencyException
+     * @throws GuzzleException
+     * @throws NetworkException
+     * @throws SodiumException
+     */
+    public function testLeadingAtSymbol(): void
+    {
+        $container = [];
+        $history = Middleware::history($container);
+        $mock = new MockHandler([
+            new Response(
+                200,
+                ['Content-Type' => 'application/json'],
+                '{"subject":"https://example.com/users/alice","links":[{"rel":"self","href":"https://example.com/users/alice"}]}'
+            )
+        ]);
+        $handlerStack = HandlerStack::create($mock);
+        $handlerStack->push($history);
+        $mockHttp = new Client(['handler' => $handlerStack]);
+
+        $webFinger = new WebFinger($this->getConfig(), $mockHttp);
+        $result = $webFinger->canonicalize('@alice@example.com');
+        $this->assertSame('https://example.com/users/alice', $result);
+        $this->assertCount(1, $container);
+        $this->assertSame(
+            'https://example.com/.well-known/webfinger?resource=acct:alice@example.com',
+            (string) $container[0]['request']->getUri()
+        );
+    }
+
+    /**
+     * @throws CacheException
+     * @throws CertaintyException
+     * @throws DependencyException
+     * @throws Exception
+     * @throws GuzzleException
+     * @throws InvalidArgumentException
+     * @throws NetworkException
+     * @throws SodiumException
+     */
+    public function testAcceptHeader(): void
+    {
+        $mockHttp = $this->createMock(Client::class);
+        $mockHttp->expects($this->once())
+            ->method('get')
+            ->with($this->anything(), $this->callback(function ($options) {
+                $this->assertArrayHasKey('Accept', $options);
+                $this->assertSame('application/activity+json', $options['Accept']);
+                return true;
+            }))
+            ->willReturn(new Response(
+                200,
+                ['Content-Type' => 'application/json'],
+                '{"inbox":"https://example.com/users/alice/inbox"}'
+            ));
+
+        $webFinger = new WebFinger($this->getConfig(), $mockHttp);
+        $webFinger->setCanonicalForTesting('https://example.com/users/alice', 'https://example.com/users/alice');
+        $webFinger->getInboxUrl('https://example.com/users/alice');
+    }
+
+    /**
+     * @throws CertaintyException
+     * @throws CryptoException
+     * @throws DependencyException
+     * @throws FetchException
+     * @throws NotImplementedException
+     * @throws SodiumException
+     */
+    public function testInvalidLinkData(): void
+    {
+        $sk = SecretKey::generate();
+        $pk = $sk->getPublicKey();
+        $mockHttp = $this->getMockClient([
+            new Response(
+                200,
+                ['Content-Type' => 'application/json'],
+                json_encode([
+                    'links' => [
+                        ['rel' => 'self', 'type' => 'application/json', 'href' => 'https://example.com/users/alice']
+                    ]
+                ])
+            ),
+            new Response(200, ['Content-Type' => 'application/activity+json'], json_encode([
+                'publicKey' => [
+                    'publicKeyPem' => $pk->encodePem()
+                ]
+            ]))
+        ]);
+        $webFinger = new WebFinger($this->getConfig(), $mockHttp);
+        $this->expectException(FetchException::class);
+        $this->expectExceptionMessage('Could not fetch public key for https://example.com/users/alice');
+        $webFinger->getPublicKey('https://example.com/users/alice');
+    }
+
+    /**
+     * @throws CertaintyException
+     * @throws CryptoException
+     * @throws DependencyException
+     * @throws FetchException
+     * @throws SodiumException
+     */
+    public function testLogicalOrMutant(): void
+    {
+        $mockHttp = $this->getMockClient([
+            new Response(
+                200,
+                ['Content-Type' => 'application/json'],
+                json_encode([
+                    'links' => [
+                        [
+                            'rel' => 'self',
+                            'type' => 'application/activity+json',
+                            'href' => 'https://example.com/users/alice'
+                        ]
+                    ]
+                ])
+            ),
+            new Response(
+                200,
+                ['Content-Type' => 'application/activity+json'],
+                '{"publicKey":{}}'
+            )
+        ]);
+        $webFinger = new WebFinger($this->getConfig(), $mockHttp);
+        $this->expectException(FetchException::class);
+        $this->expectExceptionMessage('Could not find public key for https://example.com/users/alice');
+        $webFinger->getPublicKey('https://example.com/users/alice');
     }
 }
