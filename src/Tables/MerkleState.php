b3a40480dc020b06b11ff340cf51c0c5fab1cbd4
Soatok Dreamseeker <soatok.dhole@gmail.com>
2026-01-02 17:23:52 -0500

    Revert "Fix concurrency bug"
    This reverts commit 822a5e34f075998d983f41c0136ce9df715705e1.

--- src/Tables/MerkleState.php
+++ src/Tables/MerkleState.php
@@ -347,23 +347,23 @@ class MerkleState extends Table
     protected function insertLeafInternal(MerkleLeaf $leaf, callable $inTransaction): bool
     {
         $needsTransaction = !$this->db->inTransaction();
-        switch ($this->db->getDriver()) {
-            case 'pgsql':
-            case 'mysql':
-                if ($needsTransaction) {
+        if ($needsTransaction) {
+            switch ($this->db->getDriver()) {
+                case 'pgsql':
+                case 'mysql':
                     $this->db->beginTransaction();
-                }
-                $state = $this->db->cell("SELECT merkle_state FROM pkd_merkle_state WHERE TRUE FOR UPDATE");
-                break;
-            case "sqlite":
-                if ($needsTransaction) {
+                    $state = $this->db->cell("SELECT merkle_state FROM pkd_merkle_state WHERE TRUE FOR UPDATE");
+                    break;
+                case "sqlite":
                     $this->db->exec("PRAGMA busy_timeout=5000");
                     $this->db->beginTransaction();
-                }
-                $state = $this->db->cell("SELECT merkle_state FROM pkd_merkle_state WHERE 1");
-                break;
-            default:
-                throw new NotImplementedException('Database driver support not implemented');
+                    $state = $this->db->cell("SELECT merkle_state FROM pkd_merkle_state WHERE 1");
+                    break;
+                default:
+                    throw new NotImplementedException('Database driver support not implemented');
+            }
+        } else {
+            $state = $this->db->cell("SELECT merkle_state FROM pkd_merkle_state");
         }
         $insert = empty($state);
         if ($insert) {
