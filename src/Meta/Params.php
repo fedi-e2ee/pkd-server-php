1359e500196b6678964598c7d926861dcb8c2a0f
Soatok Dreamseeker <soatok.dhole@gmail.com>
2026-01-12 19:55:52 -0500

    Prevent insecure params (#39)

--- src/Meta/Params.php
+++ src/Meta/Params.php
@@ -2,6 +2,9 @@
 declare(strict_types=1);
 namespace FediE2EE\PKDServer\Meta;
 
+use FediE2EE\PKDServer\Exceptions\DependencyException;
+use FediE2EE\PKD\Crypto\Merkle\Tree;
+
 /**
  * Server configuration parameters
  */
@@ -9,6 +12,8 @@ readonly class Params
 {
     /**
      * These parameters MUST be public and MUST have a default value
+     *
+     * @throws DependencyException
      */
     public function __construct(
         public string $hashAlgo = 'sha256',
@@ -16,5 +21,18 @@ readonly class Params
         public string $actorUsername = 'pubkeydir',
         public string $hostname = 'localhost',
         public string $cacheKey = '',
-    ) {}
+    ) {
+        if (!Tree::isHashFunctionAllowed($this->hashAlgo)) {
+            throw new DependencyException('Disallowed hash algorithm');
+        }
+        if ($this->otpMaxLife < 2) {
+            throw new DependencyException('OTP max life cannot be less than 2 seconds');
+        }
+        if ($this->otpMaxLife > 300) {
+            throw new DependencyException('OTP max life cannot be larger than 300 seconds');
+        }
+        if (!filter_var($this->hostname, FILTER_VALIDATE_DOMAIN, FILTER_FLAG_HOSTNAME)) {
+            throw new DependencyException('Hostname is not valid');
+        }
+    }
 }
---
+++ tests/Meta/ParamsTest.php
@@ -0,0 +1,72 @@
+<?php
+declare(strict_types=1);
+namespace FediE2EE\PKDServer\Tests\Meta;
+
+use FediE2EE\PKDServer\Exceptions\DependencyException;
+use FediE2EE\PKDServer\Meta\Params;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(Params::class)]
+class ParamsTest extends TestCase
+{
+    public static function hashAlgosProvider(): array
+    {
+        return [
+            ['', false],
+            ['adler32', false],
+            ['md5', false],
+            ['sha256', true],
+        ];
+    }
+
+    #[DataProvider('hashAlgosProvider')]
+    public function testInvalidHashAlgo(string $hashAlgo, bool $expectPass = false): void
+    {
+        if (!$expectPass) {
+            $this->expectException(DependencyException::class);
+        }
+        $this->assertInstanceOf(Params::class, new Params($hashAlgo));
+    }
+
+    public static function invalidMaxLifeProvider(): array
+    {
+        return [
+            [-1, false],
+            [0, false],
+            [1, false],
+            [30, true],
+            [301, false],
+            [PHP_INT_MAX, false]
+        ];
+    }
+
+    #[DataProvider("invalidMaxLifeProvider")]
+    public function testOtpLifetime(int $maxLife, bool $expectPass = false): void
+    {
+        if (!$expectPass) {
+            $this->expectException(DependencyException::class);
+        }
+        $this->assertInstanceOf(Params::class, new Params(hashAlgo: 'sha256', otpMaxLife: $maxLife));
+    }
+
+    public static function hostnameProvider(): array
+    {
+        return [
+            ['localhost', true],
+            ['furry.engineer', true],
+            ['', false],
+            ["\x0A", false],
+        ];
+    }
+
+    #[DataProvider("hostnameProvider")]
+    public function testHostname(string $host, bool $expectPass = false): void
+    {
+        if (!$expectPass) {
+            $this->expectException(DependencyException::class);
+        }
+        $this->assertInstanceOf(Params::class, new Params(hashAlgo: 'sha256', otpMaxLife: 120, hostname: $host));
+    }
+}
